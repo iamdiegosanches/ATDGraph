6dee18cd86a55a9ed84e008375c93d1f4a43f225
iamdiegosanches <dgsanches3@gmail.com>
2023-04-11 22:07:35 -0300

    Update add_directed_edge and add_undirected_edge

--- graph.h
+++ graph.h
@@ -4,21 +4,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct
-{
+typedef struct {
     int data;
-    struct Node* next;
+    struct Node *next;
 } Node;
 
-typedef struct
-{
+typedef struct {
     int count_nodes;
-    Node** adj_list;
+    Node **adj_list;
 } TGraph;
 
-TGraph* create_graph(int num_vertices);
-void add_directed_edge(TGraph * graph, int data);
-void add_undirected_edge(TGraph * graph, int data);
+TGraph *create_graph(int num_vertices);
+
+void add_directed_edge(TGraph *graph, int u, int v);
+
+void add_undirected_edge(TGraph *graph, int u, int v);
+
 void free_graph(TGraph *graph);
 
 #endif //TADGRAFO_GRAPH_H
