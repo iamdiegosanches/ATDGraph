f4e42758f9d8fb52ea1bf57eb0b69335763ee93d
iamdiegosanches <dgsanches3@gmail.com>
2023-04-14 11:45:08 -0300

    Implementing bfs

--- graph.c
+++ graph.c
@@ -25,23 +25,36 @@ void add_undirected_edge(TGraph *graph, int u, int v) {
     add_directed_edge(graph, v, u);
 }
 
-//int *bfs(TGraph *graph, int s) {
-//    TList Q;
-//    TList R;
-//    ml_empty(&Q);
-//    ml_empty(&R);
-//    int desc[graph->count_nodes];
-//    for (int i = 0; i < graph->count_nodes; ++i) {
-//        desc[i] = 0;
-//    }
-//    insert(s, &Q);
-//    insert(s, &R);
-//    desc[s] = 1;
-//
-//    while (!is_empty(Q)) {
-//        delete()
-//    }
-//}
+int *bfs(TGraph *graph, int s) {
+    int u;
+    TList Q;
+    TList R;
+    ml_empty(&Q);
+    ml_empty(&R);
+    int *desc = malloc(graph->count_nodes * sizeof(int));
+    for (int i = 0; i < graph->count_nodes; ++i) {
+        desc[i] = 0;
+    }
+    insert(s, &Q);
+    insert(s, &R);
+    desc[s] = 1;
+
+    while (!is_empty(Q)) {
+        u = delete_by_index(&Q, 0);
+        for (int v = 0; v < graph->count_nodes; ++v) {
+            if (graph->adj_list[u][v].data != -1) {
+                if (desc[v] == 0) {
+                    insert(v, &Q);
+                    insert(v, &R);
+                    desc[v] = 1;
+                }
+            }
+        }
+    }
+    free_list(&Q);
+    free_list(&R);
+    return desc;
+}
 
 void printGraph(TGraph *graph) {
     for (int i = 0; i < graph->count_nodes; ++i) {
--- main.c
+++ main.c
@@ -2,19 +2,27 @@
 
 int main() {
     TGraph *g1 = create_graph(3);
+
     TList l1;
+    int x;
+
     ml_empty(&l1);
 
     insert(1, &l1);
     insert(2, &l1);
-    delete_by_index(&l1, 0);
-    print_list(l1);
+    x = delete_by_index(&l1, 0);
+    printf("%d\n", x);
+    // print_list(l1);
 
-    add_undirected_edge(g1, 0, 1);
+    add_directed_edge(g1, 0, 1);
+    add_directed_edge(g1, 1, 2);
 
-    // printGraph(g1);
+    //int *path = bfs(g1, 0);
+
+    printGraph(g1);
 
     free_graph(g1);
+    //free(path);
     free_list(&l1);
     return 0;
 }
